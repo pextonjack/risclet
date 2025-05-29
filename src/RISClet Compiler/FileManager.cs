567e59e2a0ef9b033a246210374ce9d2d71986df
pextonjack <pextonjack@gmail.com>
2025-05-29 19:28:20 +0100

    Added FileManager class
    Added support for reading text

---
+++ src/RISClet Compiler/FileManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace RISClet_Compiler
+{
+	public class FileManager
+	{
+		public static string ReadText(string path)
+		{
+            return File.ReadAllText(path) ?? "";
+        }
+    }
+}
+
