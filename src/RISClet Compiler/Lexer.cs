372b0a012a6d0609483cc3cbfa0e176591d8e722
pextonjack <pextonjack@gmail.com>
2025-05-29 17:58:09 +0100

    Set up token class and enum

--- src/RISClet Compiler/Lexer.cs
+++ src/RISClet Compiler/Lexer.cs
@@ -9,5 +9,51 @@ namespace RISClet_Compiler
 	{
 		
 	}
+
+	public class Token
+	{
+		public TokenType Type { get; }
+		public string Lexeme { get; }
+
+		// Useful later for error handling & syntax highlighting
+		public int Line { get; }
+		public int Column { get; }
+
+		public Token(TokenType type, string lexeme, int line, int column)
+		{
+			Type = type;
+			Lexeme = lexeme;
+			Line = line;
+			Column = column;
+		}
+	}
+
+	public enum TokenType
+	{
+		// Values and identifiers
+		Identifier,
+		IntLiteral,
+		StringLiteral,
+
+		// Operators
+		Assign,
+		Plus,
+		Minus,
+		Divide,
+		Multiply,
+
+		// Punctuation
+		Colon,
+		Semicolon,
+		LeftParenth,
+		RightParenth,
+
+		// Keywords (mostly not useful in Phase 1)
+		If, Elseif, Else, While, Output, Return,
+
+		// Misc.
+		EOF,
+		Unknown
+	}
 }
 
