1c4eb4979deea52f91fb425b5f4cdf26165ce4a9
pextonjack <pextonjack@gmail.com>
2025-06-10 22:23:39 +0100

    Added if statements for each valid statement type
    Also added helper functions for converting literals and identifiers (variables) to 'DataItems'
    Also added comments for helping future me deal with temporary variable management for expressions

--- src/IR.cs
+++ src/IR.cs
@@ -6,8 +6,67 @@ namespace RISClet_Compiler
 	/// </summary>
 	public class IR
 	{
-		
-	}
+		/*
+		 * If given an expression like x: Int32 = 3 + 4; you cannot simply write this as a single IR instruction
+		 * You must instead rewrite it as something like:
+		 * t1 = 3 + 4;
+		 * x: Int32 = t1
+		 * Where t1 functions like an intermediate register for storing temporary values
+		 * BUT, you cannot ASSUME that all incoming variable assignment and declaration statements are as such...
+		 * ...SO, you must determine whether adding a temporary variable instruction is necessary for every declaration statement
+		 */
+		public static List<IRInstruction> GenerateTupleIR(ProgramNode programNode)
+		{
+			List<IRInstruction> instructions = new();
+
+            // Iterate through all statements in the program, and convert to Tuple IR
+            for (int i = 0; i < programNode.Statements.Count; i++)
+			{
+				ASTNode currentNode = programNode.Statements[i];
+
+				if (currentNode is VariableDeclarationNode varDeclare)
+				{
+					
+                }
+                else if (currentNode is AssignmentNode varAssign)
+                {
+					
+                }
+				else if (currentNode is SubroutineCallNode subCall)
+				{
+
+				}
+				else
+				{
+					ErrorReporter.CompilerError("Unexpected expression in program; only variable declaration, assignment, and subroutine calling is allowed here", (-1,-1)); // TODO: Real error location reporting
+				}
+            }
+
+			return instructions;
+		}
+
+		public static DataItem ConvertNodeToDataItem(ASTNode node)
+		{
+			if (node is IntegerLiteralNode integer)
+			{
+				return new DataItem(integer.Value);
+			}
+            else if (node is IdentifierNode ident)
+            {
+                return new DataItem(ident.Name, isIdent: true);
+            }
+			return new DataItem(0);
+        }
+
+		public static DataType GetDataType(string type)
+		{
+			return type switch
+			{
+				"Int32" => DataType.Int32,
+				_ => DataType.Int32
+			};
+		}
+    }
 
 	public abstract class IRInstruction { }
 
