37063f977eabd9a903476c8c4edd272ae30c24b5
pextonjack <pextonjack@gmail.com>
2025-06-30 22:23:26 +0100

    Added support for CLI arguments
    You can now run it from the CLI (if you add it to path especially), with the args of:
    • --version (shows version of 1B at the moment)
    • --help (shows the format and args you can use)
    • -v (makes it verbose in compilation)
    • -o <file> (choose filename and where to store, either locally or wherever you like)
    • -Wall (doesn't do anything right now, but should show warnings)
    
    For example:
    risclet prog.rlt -o prog.s
    Or
    risclet prog.rlt -o prog.s -v -Wall
    Or
    risclet --help

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RISClet_Compiler;
 
@@ -7,50 +8,196 @@ namespace RISClet_Compiler;
 /// </summary>
 class Program
 {
+    public const string VersionID = "Alpha 1B \"Metis\"";
+
     static void Main(string[] args)
     {
-        Stopwatch stopwatch = new();
-        stopwatch.Start();
+        Profile prof = ProcessArgs(args);
 
-        string src = new FileManager().ReadText("/Users/jackpexton/Desktop/risclet/code.risclet");
-        Console.WriteLine("Original source code:\n" + src + "\n");
+        switch (prof.Task)
+        {
+            case TaskType.Help:
+                Help();
+                break;
+            case TaskType.Version:
+                Version();
+                break;
+            case TaskType.Invalid:
+                Invalid();
+                break;
+            case TaskType.Compile:
+                Compile(prof);
+                break;
+        }
+    }
+
+    private static void Help()
+    {
+        Console.WriteLine("""
+            Usage: risclet <source-file> [options]
+
+            Options:
+                -o <file>       Write output to <file>
+                -v              Enable verbose mode
+                -Wall           Enable all warnings
+                --help          Show this help message
+                --version       Show version information
+            """);
+    }
+
+    private static void Version()
+    {
+        Console.WriteLine($"""
+            RISClet compiler {VersionID}
+            (c) 2025 Jack Pexton, MIT License
+            """);
+    }
+
+    private static void Invalid()
+    {
+        Console.WriteLine("""
+            Error: invalid command syntax
+            
+            Usage: risclet <source-file> [options]
+            Use 'risclet --help' for more information.
+            """);
+    }
+
+    private static void Compile(Profile profile)
+    {
+        bool verbose = profile.Verbose;
+        bool warnings = profile.ShowWarnings;
+
+        Stopwatch stopwatch = new();
+
+        if (verbose)
+        {
+            stopwatch.Start();
+        }
+
+        string filePath = Path.GetFullPath(profile.SourceFilename);
+        string src = new FileManager().ReadText(filePath);
+        if (verbose)
+        {
+            Console.WriteLine($"RISClet Source Code, from \"{filePath}\":\n{src}\n");
+        }
 
         var tokens = new Lexer().Tokenise(src);
-        Console.WriteLine("Tokens:");
-        foreach (var token in tokens)
+        if (verbose)
         {
-            Console.Write(token.ToString() + " ");
+            System.Text.StringBuilder tokenString = new();
+            foreach (Token t in tokens) tokenString.Append(t.ToString() + " ");
+
+            Console.WriteLine($"Tokens:\n{tokenString}\n");
         }
 
-        Console.WriteLine("\n\nAST:");
         var ast = new Parser().Parse(tokens);
-        foreach (ASTNode n in ast.Statements)
+        if (verbose)
         {
-            Console.WriteLine(n.GetType().ToString());
+            System.Text.StringBuilder astString = new();
+            foreach (ASTNode node in ast.Statements) astString.Append(node.GetType().ToString() + "\n");
+
+            Console.WriteLine($"AST Nodes:\n{astString}\n");
         }
 
-        Console.WriteLine("\n\nTuple IR:");
-        var tupleIr = new TupleIR().GenerateTupleIR(ast);
-        Console.WriteLine(tupleIr.ToString());
-
-        //Console.WriteLine("\n\nAssembly:");
-        //var code = new CodeGenerator().GenerateCode(ir);
-        //Console.WriteLine(code);
-
-        Console.WriteLine("\n\nLower IR:");
-        var lowerIr = new LowerIR().GenerateLowerIR(tupleIr);
-        foreach (LowerIRInstruction instruction in lowerIr.Instructions)
+        var tupleIR = new TupleIR().GenerateTupleIR(ast);
+        if (verbose)
         {
-            Console.WriteLine(instruction.GetType().ToString());
+            Console.WriteLine($"Tuple IR:\n{tupleIR}\n");
         }
 
-        Console.WriteLine("\n\nAssembly:");
-        var code = new CodeGenerator().GenerateCode(lowerIr);
-        Console.WriteLine(code);
+        var lowerIR = new LowerIR().GenerateLowerIR(tupleIR);
+        if (verbose)
+        {
+            System.Text.StringBuilder lowerIRString = new();
+            foreach (LowerIRInstruction lowerInstruction in lowerIR.Instructions) lowerIRString.Append(lowerInstruction.ToString() + "\n");
 
-        new FileManager().SaveText("/Users/jackpexton/Desktop/risclet/prog.s", code);
-        Console.WriteLine("\nCompilation: Done!");
-        stopwatch.Stop();
-        Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Lower IR:\n{lowerIRString}\n");
+        }
+
+        var code = new CodeGenerator().GenerateCode(lowerIR);
+        if (verbose)
+        {
+            Console.WriteLine($"AArch64:\n{code}\n");
+        }
+
+        new FileManager().SaveText(Path.GetFullPath(profile.DestinationFilename ?? "null.s"), code);
+        Console.WriteLine($"Compiled, ready for assembly, in {Path.GetFullPath(profile.DestinationFilename ?? "null.s")}");
+
+        if (verbose)
+        {
+            stopwatch.Stop();
+            Console.WriteLine($"Completed in {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+
+    private static Profile ProcessArgs(string[] args)
+    {
+        // Case 0: args is empty
+        if (args.Length == 0)
+            return new Profile() { Task = TaskType.Invalid };
+
+        // Special cases
+        // Case 1: --help
+        if (args[0] == "--help")
+            return new Profile() { Task = TaskType.Help };
+
+        // Case 2: --version
+        if (args[0] == "--version")
+            return new Profile() { Task = TaskType.Version };
+
+        // args[0] assumed to be the filename
+        var profile = new Profile() { SourceFilename = args[0], Task = TaskType.Compile };
+
+        for (int i = 1; i < args.Length; i++)
+        {
+            if (args[i] == "-o" && i + 1 < args.Length)
+            {
+                profile.DestinationFilename = args[i + 1];
+            }
+            else if (args[i] == "-v")
+            {
+                profile.Verbose = true;
+            }
+            else if (args[i] == "-Wall")
+            {
+                profile.ShowWarnings = true;
+            }
+        }
+
+        if (profile.DestinationFilename == null)
+        {
+            profile.DestinationFilename = profile.SourceFilename.Split('.')[0] + ".s";
+        }
+
+        return profile;
+    }
+
+    private struct Profile
+    {
+        public string SourceFilename;
+        public string? DestinationFilename;
+
+        public bool Verbose;
+        public bool ShowWarnings;
+
+        public TaskType Task;
+
+        public Profile()
+        {
+            SourceFilename = "";
+            DestinationFilename = null;
+            Verbose = false;
+            ShowWarnings = false;
+            Task = TaskType.Invalid;
+        }
+    }
+
+    private enum TaskType
+    {
+        Compile,
+        Help,
+        Version,
+        Invalid
     }
 }
